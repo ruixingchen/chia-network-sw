f1f5f23c52b0bd33d323cbe8cbaf5257125e43e1
ruixingchen <dev@ruixingchen.com>
2023-02-18 01:22:49 +0800

    尝试支持多实例

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -6,3 +6,15 @@ from pathlib import Path
 DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_ROOT", "~/.chia/mainnet"))).resolve()
 
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("CHIA_KEYS_ROOT", "~/.chia_keys"))).resolve()
+
+bin_path = Path(os.path[0])
+print(f"获取到当前启动路径： {bin_path}")
+config_file = bin_path/"config_root.json"
+if config_file.exists():
+    _json = json.loads(config_file.read_bytes())
+    root = _json["DEFAULT_ROOT_PATH"]
+    if root and len(root) > 1:
+        DEFAULT_ROOT_PATH = Path(root).expanduser().resolve()
+    keys_root = _json["DEFAULT_KEYS_ROOT_PATH"]
+    if keys_root and len(keys_root) > 1:
+        DEFAULT_KEYS_ROOT_PATH = Path(keys_root).expanduser().resolve()
